86487fe399723d8328661b81c30f0eaa7a0db169
CakeBrewery <cakebrewery@gmail.com>
2015-02-17 05:33:38 +0000

    added some stock images to event cards

--- client/app/scripts/controllers/events.js
+++ client/app/scripts/controllers/events.js
@@ -6,18 +6,21 @@ angular.module('ts300App')
 	    {
 	    	name: "Free Food", 
 	    	description: "There is going to be free food in this event",
-	    	location: "1010 nice street"
+	    	location: "1010 nice street",
+	    	img_url: "images/events/img1.jpg"
 	    }, 
 
 	    {
 	    	name: "Awesome Mt. Tolmie Hike", 
 	    	description: "It's a hike a Mt. Tolmie! Feel free to come", 
-	    	location: "Mount Tolmie"
+	    	location: "Mount Tolmie",
+	    	img_url: "images/events/img2.jpg"
 	    },
 	    {
 	    	name: "Beach Volleyball Party at Willows", 
 	    	description: "Come to Willows beach and play volleyball with us! ", 
-	    	location: "Willows Beach"
+	    	location: "Willows Beach",
+	    	img_url: "images/events/img3.jpg"
 	    }
     ];
   });
