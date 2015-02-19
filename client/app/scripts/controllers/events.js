f57b44d21c9b7e614560c99b9e80871c335529ae
CakeBrewery <cakebrewery@gmail.com>
2015-02-19 08:04:43 +0000

    fixed more css bs

--- client/app/scripts/controllers/events.js
+++ client/app/scripts/controllers/events.js
@@ -16,28 +16,28 @@ angular.module('ts300App')
 	   		id: 2,
 	    	name: "Beacon Hill Petting Zoo",
 	    	description: "Come to beacon hill and interact with fluffy animals!",
-	    	location: "Beacon Hill Park",
+	    	location: "Beacon Hill Park, Circle Drive, Victoria, BC V8V",
 	    	img_url: "images/events/img5.jpg"
 	    },
 	    {
 	    	id: 3,
 	    	name: "Awesome Mt. Tolmie Hike", 
 	    	description: "It's a group hike a Mt. Tolmie! Feel free to come", 
-	    	location: "Mount Tolmie",
+	    	location: "Mount Tolmie, Victoria, BC V8P 4R6",
 	    	img_url: "images/events/img2.jpg"
 	    },
 	    {
 	    	id: 4,
 	    	name: "Beach Volleyball Party at Willows", 
 	    	description: "Come to Willows beach and play volleyball with us! ", 
-	    	location: "Willows Beach",
+	    	location: "Willows Beach, Victoria, BC",
 	    	img_url: "images/events/img3.jpg"
 	    },
 	    {
 	    	id: 5,
 	    	name: "Hot Dogs for Free @ UVic",
 	    	description: "We are giving out free hot dogs at UVic, come and get yours!",
-	    	location: "Outdie the SUB at UVic",
+	    	location: "Outside the SUB at UVic, Victoria BC",
 	    	img_url: "images/events/img4.jpg"
 	    }
     ];
@@ -47,14 +47,14 @@ angular.module('ts300App')
 	    	id: 2,
 	    	name: "beacon Hill Petting Zoo",
 	    	description: "Come to beacon hill and interact with fluffy animals!",
-	    	location: "Beacon Hill Park",
+	    	location: "Beacon Hill Park, Circle Drive, Victoria, BC V8V",
 	    	img_url: "images/events/img5.jpg"
 	    },
 	    {
 	    	id: 3,
 	    	name: "Awesome Mt. Tolmie Hike", 
 	    	description: "It's a group hike a Mt. Tolmie! Feel free to come", 
-	    	location: "Mount Tolmie",
+	    	location: "Mount Tolmie, Victoria, BC V8P 4R6",
 	    	img_url: "images/events/img2.jpg"
 	    }
 	];
